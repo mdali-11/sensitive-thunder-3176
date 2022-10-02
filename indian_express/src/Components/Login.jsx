412da581c8114608818c4ed76632d67d8cd997db
Md Ali <mdali91935@gmail.com>
2022-10-02 10:00:22 +0530

    added home containor added proper login functionality etch the news api and rectify the navbar

--- indian_express/src/Components/Login.jsx
+++ indian_express/src/Components/Login.jsx
@@ -1,87 +1,150 @@
+import React, { useContext, useEffect, useState } from "react";
+import style from "./Login.module.css";
+import { AppContext } from "../Context/AppContext";
+import {
+  Stack,
+  Box,
+  Heading,
+  Text,
+  Input,
+  Flex,
+  Spacer,
+  Image,
+  Button
 
-import { useState, useEffect } from "react";
-import style from "../Components/Login.module.css";
-import { Stack, Box, Heading, Text, Input, Button } from "@chakra-ui/react";
+} from "@chakra-ui/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook, FaApple } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 
 const getData = () => {
-  const data = JSON.parse(localStorage.getItem("userData"));
-
-  return data ? data : [];
+  const data = JSON.parse(localStorage.getItem("userInfoCreds"));
+  
+  if (data) {
+    // console.log("data", data);
+    return data;
+  } else {
+    return [];
+  }
 };
 
-const Login = () => {
-  useEffect(() => {
-    document.title = "Latest News,India Breaking News";
-  }, []);
 
+const Login = () => {
+const {handleisAuth}=useContext(AppContext)
+
+  useEffect(() => {
+    document.title = "Login Page: Latest News, India News, Breaking News, Today's News Headlines Online, English News Top Stories, Coronavirus News, IPL 2022 Updates|The Indian Express";
+  }, [])
   const navigate = useNavigate();
   const [submit, setSubmit] = useState(getData());
-  const [login, setLogin] = useState([]);
-  const [IsUser, setIsUser] = useState(false);
-  const [signInData, setSignInData] = useState({
+  // const [login, setLogin] = useState([]);
+  const [isUser, setIsUser] = useState(false);
+  const [signInCreds, setSignInCreds] = useState({
     email: "",
     password: "",
     confirmPassword: "",
     id: Date.now(),
   });
 
-  const [loginData, setLoginData] = useState({
-    email: "",
-    password: "",
-  });
+  // const [loginCreds, setLoginCreds] = useState({
+  //   email: "",
+  //   password: "",
+  // });
+
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("");
 
   const HandleSignIn = (e) => {
-    const { name, value } = e.target;
-    setSignInData({
-      ...signInData,
+    let { name, value } = e.target;
+    // console.log( type, name, value);
+
+    setSignInCreds({
+      ...signInCreds,
       [name]: value,
     });
   };
 
   const HandleSignInSubmit = () => {
-    setSubmit({ ...submit, signInData });
-    setSignInData("");
+    setSubmit([...submit, signInCreds]);
+    setSignInCreds("");
     setIsUser(true);
-   alert("Sign inSuccessfull")
+    alert("Thank! You're Successfully Signed In to Indian Express");
   };
 
   useEffect(() => {
-    localStorage.setItem("userData", JSON.stringify(submit));
+    localStorage.setItem("userInfoCreds", JSON.stringify(submit));
   }, [submit]);
 
-  const HandleLogin = (e) => {
-    const { name, value } = e.target;
-
-    setLoginData({ ...loginData, [name]: value });
-  };
+  // const HandleLogin = (e) => {
+  //   let { name, value } = e.target;
+  //   // console.log( name, value);
+  //   setLoginCreds({
+  //     ...loginCreds,
+  //     [name]: value,
+  //   });
+  // };
+  let obj={
+    email,
+    password,
+  }
 
   const HandleLogInSubmit = () => {
-    console.log(submit, login);
-    setLogin({ ...login, loginData });
-    setLoginData("");
+    console.log("submit", submit);
 
-    for (let i = 0; i < submit.length; i++) {
-      if (
-        login[0].email === submit[i].email &&
-        login[0].password === submit[i].password
-      ) {
-        alert("Login Succesfull")
-        navigate("/");
-      } else {
-        alert("Please Enter Correct Details")
-      }
-    }
-  };
+    if(check(submit,obj))
+    {
+            alert(`Thank! You're Successfully Signed In to Indian Express`);
+            handleisAuth();
+            navigate("/");
+            
+          } else {
+            alert(`Wrong Email Or Password`);
+          }
+   
+    // setLogin([...login, loginCreds]);
+    //  console.log("Login", login);
+    // setLoginCreds("");
 
-  const goToLogin = () => {
+  }
+
+  // if(login.length!==0){
+
+  //   setTimeout(()=>{
+  //     logit(submit,login)
+  //   },2000)
+  //   return ;
+  // }
+
+
+  
+  // function logit(submit,login){
+  //     if (
+  //        check(submit,login)
+  //     ) {
+  //       alert(`Thank! You're Successfully Signed In to Indian Express`);
+  //       navigate("/");
+        
+  //     } else {
+  //       alert(`Wrong Email Or Password`);
+  //     }
+  
+  // };
+
+
+
+
+
+
+
+
+  
+
+  const GotoLogin = () => {
     setIsUser(false);
   };
 
-  const goToSignin = () => {
+  const GoToSignIn = () => {
     setIsUser(true);
   };
 
@@ -91,182 +154,212 @@ const Login = () => {
         <div className={style.backbtn}>
           <ArrowBackIcon />
           <Button>
-            <Link to="/">Back to Indian Express</Link>
+            <Link to="/">Back to IndianExpress</Link>
           </Button>
         </div>
 
         <div>
-        <Link to="/"><img
+          <img
             src="	https://indianexpress.com/wp-content/themes/indianexpress/images/evolok/indian-express-logo-n.svg"
-            alt="Indian-express-logo"
-          /></Link>
+            alt="IndianExpress"
+          />
         </div>
+      </div>
+
+      <Flex style={{marginTop:"50px", marginBottom:"50px"}} w="80%" margin="auto" h="250px">
+    <Box w="55%">
+      <Heading>Express full access package offers e-paper + premium</Heading>
+      <Text mt={5}>Full access to all journalism of The Indian Express</Text>
+       <Text>Access to ePaper archives</Text>
+       <Text>Invites to * premium IE events*</Text>
+       <Text>Access to all premium stories</Text>
+       <Text>Access to all exclusive newsletters</Text>
+       <Text>Ad-lite experience across platforms</Text>
+
+       <Text mt={15}>For multi-user and corporate subscriptions, Click here</Text>
+
+    </Box>
+    <Spacer />
+    <Box W="100%">
+    <Image width="450px" src="https://indianexpress.com/wp-content/themes/indianexpress/images/evolok/Laptop_header-image_1.1.png"/>
+
+    </Box>
+
+    </Flex>
+      <div className={style.LoginContainor}>
+        <div className={style.LoginForm}>
+          <Heading as="h2" size="md" noOfLines={1}>
+            WELCOME BACK
+          </Heading>
+          <Stack spacing={4}>
+            <Box
+              textAlign="center"
+              bg="#d6d3d32e"
+              w="100%"
+              p={2}
+              border="1px solid #cfcfcf"
+              borderRadius="12px"
+            >
+              <div className={style.IconDiv}>
+                <FcGoogle /> Continue with Google
+              </div>
+            </Box>
+
+            <Box
+              textAlign="center"
+              bg="#d6d3d32e"
+              w="100%"
+              p={2}
+              border="1px solid #cfcfcf"
+              borderRadius="12px"
+            >
+              <div className={style.IconDiv}>
+                <FaFacebook color="rgb(1, 1, 145)" /> Continue with Facebook
+              </div>
+            </Box>
+            <Box
+              textAlign="center"
+              bg="#d6d3d32e"
+              w="100%"
+              p={2}
+              border="1px solid #cfcfcf"
+              borderRadius="12px"
+            >
+              <div className={style.IconDiv}>
+                <FaApple color="black" />
+                Continue with Apple
+              </div>
+            </Box>
+          </Stack>
+
+          <div className={style.orDiv}>
+            <div></div>
+            <div>or</div>
+            <div></div>
+          </div>
         </div>
 
-      
-          <div className={style.LoginContainor}>
-            <div className={style.LoginForm}>
-              <Heading as="h2" size="md" noOfLines={1}>
-                WELCOME BACK
-              </Heading>
-              <Stack spacing={4}>
-                <Box
-                  textAlign="center"
-                  bg="#d6d3d32e"
-                  w="100%"
-                  p={2}
-                  border="1px solid #cfcfcf"
-                  borderRadius="12px"
-                >
-                  <div className={style.IconDiv}>
-                    <FcGoogle /> Continue with Google
-                  </div>
-                </Box>
+        {!isUser ? (
+          <div className={style.AlreadySiginn}>
+            <Stack>
+              <Text
+                fontSize="md"
+                w="90%"
+                margin="auto"
+                color="rgb(109 109 147)"
+              >
+                Enter the email address or mobile number associated with your
+                account to sign in.
+              </Text>
+            </Stack>
+            <div className={style.InputDiv}>
+              <Input
+                variant="flushed"
+                name="email"
+                value={email}
+                placeholder="Insert your registered Email/Phone"
+                type="email"
+                onChange={(e)=>setEmail(e.target.value)}
+              />
+              <Input
+                variant="flushed"
+                name="password"
+                value={password}
+                placeholder="Password"
+                type="password"
+                onChange={(e)=>setPassword(e.target.value)}
+              />
+            </div>
 
-                <Box
-                  textAlign="center"
-                  bg="#d6d3d32e"
-                  w="100%"
-                  p={2}
-                  border="1px solid #cfcfcf"
-                  borderRadius="12px"
-                >
-                  <div className={style.IconDiv}>
-                    <FaFacebook /> Continue with Facebook
-                  </div>
-                </Box>
+            <div className={style.SubmitButton}>
+              <Button size="lg" w="100%" onClick={HandleLogInSubmit}>
+                Log In
+              </Button>
+            </div>
 
-                <Box
-                  textAlign="center"
-                  bg="#d6d3d32e"
-                  w="100%"
-                  p={2}
-                  border="1px solid #cfcfcf"
-                  borderRadius="12px"
-                >
-                  <div className={style.IconDiv}>
-                    <FaApple color="black" /> Continue with Apple
-                  </div>
-                </Box>
-              </Stack>
-              <div className={style.orDiv}>
-                <div></div>
-                <div>Or</div>
-                <div></div>
+            <div className={style.TakingtoSignIn}>
+              <div>
+                <Text color="gray.500">New to Indian Express?</Text>
+              </div>
+              <div onClick={GoToSignIn}>
+                <h1>Create an account to get started</h1>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className={style.NeedToSiginn}>
+            <div className={style.InputDiv2}>
+              <Input
+                variant="flushed"
+                name="email"
+                value={signInCreds.email}
+                placeholder="Email Address"
+                type="email"
+                onChange={HandleSignIn}
+              />
+              <Input
+                variant="flushed"
+                name="password"
+                value={signInCreds.password}
+                placeholder="Password"
+                type="password"
+                onChange={HandleSignIn}
+              />
+              <Input
+                variant="flushed"
+                name="confirmPassword"
+                value={signInCreds.confirmPassword}
+                placeholder="Confirm Password"
+                type="password"
+                onChange={HandleSignIn}
+              />
+            </div>
+
+            <div className={style.TC}>
+              <div>
+                <input type="checkbox" />
+                <p>
+                  Yes, I'd like to receive The Indian Express daily newsletter.
+                </p>
+              </div>
+              <div>
+                <input type="checkbox" />
+                <p>Accept Terms and Conditions and our privacy policy</p>
               </div>
             </div>
 
-            {!IsUser ? (
-              <div className={style.AlreadySiginn}>
-                <Stack>
-                  <Text
-                    fontSize="md"
-                    w="90%"
-                    margin="auto"
-                    color="rgb(109 109 147"
-                  >
-                    Enter the email address or mobile number associated with
-                    your account to sign in.
-                  </Text>
-                </Stack>
-                <div className={style.InputDiv}>
-                  <Input
-                    variant="flushed"
-                    name="email"
-                    value={loginData.email}
-                    placeholder="Enter your registered email/phone"
-                    type="email"
-                    onChange={HandleLogin}
-                  />
-                  <Input
-                    variant="flushed"
-                    name="password"
-                    value={loginData.password}
-                    placeholder="Password"
-                    type="password"
-                    onChange={HandleLogin}
-                  />
-                </div>
+            <div className={style.SubmitButton}>
+              <Button size="lg" w="100%" onClick={HandleSignInSubmit}>
+                Register
+              </Button>
+            </div>
 
-                <div className={style.SubmitButton}>
-                  <Button size="lg" w="100%" onClick={HandleLogInSubmit}>
-                    Log In
-                  </Button>
-                </div>
-
-                <div className={style.TakingToSignIn}>
-                  <div>
-                    <Text color="gray.500">New to Indian Express ?</Text>
-                  </div>
-                  <div onClick={goToSignin}>
-                    <h1>Create an account to get Started</h1>
-                  </div>
-                </div>
+            <div className={style.TakingtoSignIn}>
+              <div>
+                <Text color="gray.500">Already Have an Account?</Text>
               </div>
-            ) : (
-              <div className={style.NeedToSiginn}>
-                <div className={style.InputDiv2}>
-                  <Input
-                    variant="flushed"
-                    name="email"
-                    type="email"
-                    value={signInData.email}
-                    placeholder="Emsil Address"
-                    onChange={HandleSignIn}
-                  />
-                  <Input
-                    variant="flushed"
-                    name="password"
-                    placeholder="Password"
-                    type="password"
-                    value={signInData.password}
-                    onChange={HandleSignIn}
-                  />
-                  <Input
-                    variant="flushed"
-                    name="confirmPassword"
-                    placeholder="Confirm Password"
-                    type="password"
-                    value={signInData.password}
-                    onChange={HandleSignIn}
-                  />
-                </div>
-
-                <div className={style.TC}>
-                  <div>
-                    <input type="checkbox" />
-                    <p>
-                      Yes, I'd like to receive The Indian Express daily
-                      newsletter.
-                    </p>
-                  </div>
-                  <div>
-                    <input type="checkbox" />
-                    <p>Accept Terms and Conditions and our privacy policy</p>
-                  </div>
-                  </div>
-
-                  <div className={style.SubmitButton}>
-                    <Button size="lg" w="100%" onClick={HandleSignInSubmit}>
-                      Register
-                    </Button>
-                  </div>
-
-                  <div className={style.TakingToSignIn}>
-                    <div>
-                      <Text color="gray.500">Already have an account ?</Text>
-                    </div>
-                    <div onClick={goToLogin}>
-                      <h1>Sign In</h1>
-                    </div>
-                  </div>
-                </div>
-            )}
+              <div onClick={GotoLogin}>
+                <h1>Sign in</h1>
+              </div>
+            </div>
           </div>
-        </div>
-      
-    
+        )}
+      </div>
+    </div>
   );
 };
 
 export default Login;
+
+function check(submit,login){
+  for (let i = 0; i < submit.length; i++) {
+    if (
+      login.email === submit[i].email &&
+      login.password === submit[i].password
+    ){
+      return true;
+    }
+    return false;
+  }
+
+}
--- indian_express/src/Components/Navbar.jsx
+++ indian_express/src/Components/Navbar.jsx
@@ -1,36 +1,40 @@
 // import { useRef } from "react";
 import { Link } from "react-router-dom";
+import { Text } from "@chakra-ui/react";
 import style from "../Components/Navbar.module.css";
+import { useContext } from "react";
 import Footer from "./Footer";
+import { AppContext } from "../Context/AppContext";
+import Home from "./Home";
 import {
     Box,
-    Spacer ,
+    Spacer,
     Menu,
     MenuButton,
     MenuList,
 } from '@chakra-ui/react'
-import {   Flex, Button,  HStack , chakra} from '@chakra-ui/react';
+import { Flex, Button, HStack, chakra } from '@chakra-ui/react';
 import React from "react";
 import {
-    HamburgerIcon,CloseIcon,
+    HamburgerIcon, CloseIcon,
     // ArrowBackIcon
 } from "@chakra-ui/icons";
 
-const data=[
+const data = [
     {
-        label:"Home",
+        label: "Home",
     },
     {
-        label:"Home",
+        label: "English",
     }, {
-        label:"Home",
+        label: "Hindi",
     }, {
-        label:"Home",
+        label: "Bengali",
     },
 ]
 // const date=Date.now().toLocaleString();
-let current_date=new Date();
-let e_date=current_date.toDateString()
+let current_date = new Date();
+let e_date = current_date.toDateString()
 
 const CTA = "Get Started"
 
@@ -38,13 +42,14 @@ const CTA = "Get Started"
 export default function Navbar() {
 
 
+    const {isAuth}=useContext(AppContext)
 
 
     return (
         <div>
             <div className={style.NavbarMainDiv}>
                 <div className={style.MenuIconBox}>
-                    <div style={{display:"flex",justifyContent:"center", alignItems:"center"}}>
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                         <Menu>
                             {({ isOpen }) => (
                                 <>
@@ -133,33 +138,32 @@ export default function Navbar() {
                                 </>
                             )}
                         </Menu>
-
                         <chakra.header id="header">
-      <Flex
-        w="full"
-        px="3"
-        py="3"
-        align="center"
-        justify="space-between"
-      >
-      
-        <HStack as="nav" spacing="5">
-          {data.map((item, i) => (
-            <Link key={i}>
-              <Button variant="nav"> {item.label} </Button>
-            </Link>
-          ))}
-        </HStack>
-        <HStack>
-          <Button color="white"
-          colorScheme="red"
-          marginLeft="800px">
-            {CTA}
-          </Button>
-        </HStack>
-        
-      </Flex>
-    </chakra.header>
+                            <Flex
+                                w="full"
+                                px="3"
+                                py="3"
+                                align="center"
+                                justify="space-between"
+                            >
+                                <Box spacing="5">
+                                    {data.map((item, i) => (
+                                        <Link key={i}>
+                                            <Button variant="nav"> {item.label} </Button>
+                                        </Link>
+                                    ))}
+                                </Box>
+                                <Spacer />
+                                <HStack>
+                                    <Button color="white"
+                                        colorScheme="red"
+                                        marginLeft="800px">
+                                        {CTA}
+                                    </Button>
+                                </HStack>
+
+                            </Flex>
+                        </chakra.header>
                     </div>
 
 
@@ -176,46 +180,49 @@ export default function Navbar() {
             <div className={style.HeadLogo}>
                 <div className={style.backbtn}>
                     {/* <ArrowBackIcon /> */}
-                    <Button>
+                    <Box>
                         <Link to="#">{e_date}</Link>
-                    </Button>
+                    <Text fontSize={12} fontWeight="10px">EPAPER | TODAY’S PAPER</Text>
+                    </Box>
                 </div>
 
-                <div><Link to ="/">
+                <Box paddingTop={5}><Link to="/">
                     <img
-                        src="	https://indianexpress.com/wp-content/themes/indianexpress/images/evolok/indian-express-logo-n.svg"
+                        src="https://indianexpress.com/wp-content/themes/indianexpress/images/indian-express-logo-n.svg"
                         alt="Indian-express-logo"
-                    /></Link>
-                </div>
+                    />
+                    </Link>
+                    <Text fontWeight="bold" mt={1} fontSize={15} ml={65}>JOURNALISM OF COURAGE</Text>
+                </Box>
             </div>
-             
 
-            <Flex  alignItems="center" justify="center" py={2} bg="gray.50" my={0.5}>
-                <Box style={{display:"flex",gap:"10px",marginLeft:"25px"}}>
-                <Link  to="#">Home</Link>
-                <Link to="#">Explained</Link>
-                <Link to="#">Political Pulse</Link>
-                <Link to="#">India</Link>
-                <Link to="#">Cities</Link>
-                <Link to="#">Opinion</Link>
-                <Link to="#">Entertainment</Link>
-                <Link to="#">Lifestyle</Link>
-                <Link to="#">Technology</Link>
-                <Link to="#">Video</Link>
-                <Link to="#">Sports</Link>
-                <Link to="#">Audio</Link>
-                <Link to="#">Education</Link>
-                <Link to="#">Premium</Link>
-                <Link to="#">Investigation</Link>
+
+            <Flex alignItems="center" justify="center" py={2} bg="gray.50" my={0.5}>
+                <Box style={{ display: "flex", gap: "10px", marginLeft: "25px" }}>
+                    <Link to="#">Home</Link>
+                    <Link to="#">Explained</Link>
+                    <Link to="#">Political Pulse</Link>
+                    <Link to="#">India</Link>
+                    <Link to="#">Cities</Link>
+                    <Link to="#">Opinion</Link>
+                    <Link to="#">Entertainment</Link>
+                    <Link to="#">Lifestyle</Link>
+                    <Link to="#">Technology</Link>
+                    <Link to="#">Video</Link>
+                    <Link to="#">Sports</Link>
+                    <Link to="#">Audio</Link>
+                    <Link to="#">Education</Link>
+                    <Link to="#">Premium</Link>
+                    <Link to="#">Investigation</Link>
                 </Box>
-              <Spacer />
-                <Box gap="15px"> 
+                <Spacer />
+                <Box gap="15px">
                     <Button color="black" bg="orange.300" mx={2}>Subscribe</Button>
-                   <Link to="login"> <Button color="black" bg="orange.300" mx={2}>Login</Button></Link>
-                 
+                    <Link to="login"> <Button color="black" bg="orange.300" mx={2}>{isAuth?"My Account" : "Sign In"}</Button></Link>
+
                 </Box>
 
-             </Flex> 
+            </Flex>
             {/* <div style={{ display: "flex", gap: "20px", marginLeft: "70px" }}>
                 <Link to="#">Home</Link>
                 <Link to="#">Explained</Link>
@@ -235,9 +242,9 @@ export default function Navbar() {
             </div> */}
 
 
+            <Home />
 
-
-<Footer />
+            <Footer />
 
         </div>
     )
